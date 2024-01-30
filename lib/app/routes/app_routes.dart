e752a2933f9476364f63b51173d98e2ffa7d4c06
dyoputra <bengawanputra21@gmail.com>
2024-01-30 15:03:38 +0700

    tugasupdate

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,20 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const BOOK = _Paths.BOOK;
+  static const ADD_BOOK = _Paths.ADD_BOOK;
+  static const PEMINJAMAN = _Paths.PEMINJAMAN;
+  static const LOGIN = _Paths.LOGIN;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const BOOK = '/book';
+  static const ADD_BOOK = '/add-book';
+  static const PEMINJAMAN = '/peminjaman';
+  static const LOGIN = '/login';
+}
